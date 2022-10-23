8fd1859aa89255cc5062a4e35aa3d6bf44ac3c59
jossemii <josemi.bnf@gmail.com>
2022-10-23 10:46:57 +0200

    ads

--- setup.py
+++ setup.py
@@ -2,16 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='grpcbigbuffer',
-    version='__dev__',
+    version='0.0.1',
 
-    url='https://github.com/jossemii/GRPCBigBuffer_Client-Python.git',
+    url='https://github.com/jossemii/GRPCBigBuffer.git',
     author='Josemi Avellana',
     author_email='josemi.bnf@gmail.com',
 
     py_modules=['grpcbigbuffer', 'buffer_pb2'],
     install_requires=[
-        'grpcio==1.34.1',
-        'grpcio-tools==1.34.1',
+        'grpcio==1.48.1',
         'protobuf==3.19.4',
     ],
     package_dir={"": "src"},
