8b515a0f89ba9f5cf134261f63ccf5d8189044d5
josemi <josemiguel@m.e-tecnia.es>
2022-12-17 17:33:47 +0100

    asd

--- src/grpcbigbuffer/block_builder.py
+++ src/grpcbigbuffer/block_builder.py
@@ -1,7 +1,3 @@
-import sys;
-
-sys.path.append('../')
-
 import warnings
 from typing import Any, List, Dict
 from grpcbigbuffer import buffer_pb2
@@ -21,6 +17,7 @@ def is_block(bytes_obj, blocks: List[bytes]):
     except DecodeError:
         pass
     return False
+
 
 def get_hash(block: buffer_pb2.Buffer.Block) -> str:
     from hashlib import sha3_256
@@ -73,42 +70,3 @@ def build_multiblock(
         blocks=blocks
     )
     return container
-
-
-from grpcbigbuffer.test_pb2 import Test
-
-block = buffer_pb2.Buffer.Block()
-h = buffer_pb2.Buffer.Block.Hash()
-h.type = b'sha256'
-h.value = b'sha512'
-block.hashes.append(h)
-
-block2 = buffer_pb2.Buffer.Block()
-h = buffer_pb2.Buffer.Block.Hash()
-h.type = b'sha256'
-h.value = b'sha256'
-block2.hashes.append(h)
-
-block3 = buffer_pb2.Buffer.Block()
-h = buffer_pb2.Buffer.Block.Hash()
-h.type = b'sha256'
-h.value = b'sha3256'
-block3.hashes.append(h)
-
-b = Test()
-b.t1 = block2.SerializeToString()
-b.t2 = block.SerializeToString()
-
-c = Test()
-c.t1 = block3.SerializeToString()
-c.t2 = b'adios'
-c.t3.CopyFrom(b)
-
-print(
-    build_multiblock(
-        pf_object_with_block_pointers=c,
-        blocks=[b'sha256', b'sha512', b'sha3256']
-    )
-)
-print(c.SerializeToString())
-
--- test/block_builder.py
+++ test/block_builder.py
@@ -1,8 +1,53 @@
-import unittest
+import sys, unittest
+
+sys.path.append('../src/')
+
+from grpcbigbuffer import buffer_pb2
+from grpcbigbuffer.block_builder import build_multiblock
+
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
+        from grpcbigbuffer.test_pb2 import Test
+
+        block = buffer_pb2.Buffer.Block()
+        h = buffer_pb2.Buffer.Block.Hash()
+        h.type = b'sha256'
+        h.value = b'sha512'
+        block.hashes.append(h)
+
+        block2 = buffer_pb2.Buffer.Block()
+        h = buffer_pb2.Buffer.Block.Hash()
+        h.type = b'sha256'
+        h.value = b'sha256'
+        block2.hashes.append(h)
+
+        block3 = buffer_pb2.Buffer.Block()
+        h = buffer_pb2.Buffer.Block.Hash()
+        h.type = b'sha256'
+        h.value = b'sha3256'
+        block3.hashes.append(h)
+
+        b = Test()
+        b.t1 = block2.SerializeToString()
+        b.t2 = block.SerializeToString()
+
+        c = Test()
+        c.t1 = block3.SerializeToString()
+        c.t2 = b'adios'
+        c.t3.CopyFrom(b)
+
+        print(
+            build_multiblock(
+                pf_object_with_block_pointers=c,
+                blocks=[b'sha256', b'sha512', b'sha3256']
+            )
+        )
+        print(c.SerializeToString())
+        print('\n')
+
         self.assertEqual(True, False)  # add assertion here
+
 
 if __name__ == '__main__':
     unittest.main()
