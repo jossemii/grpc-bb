8efbb9e84a29331faab99f67d4323c43dc8bc95b
Josemi <josemiguel.avellana@m.e-tecnia.es>
2023-01-12 18:51:23 +0100

    fda

--- src/grpcbigbuffer/client.py
+++ src/grpcbigbuffer/client.py
@@ -964,7 +964,8 @@ def serialize_to_buffer(
                     for b in send_file(
                             filedir=partition,
                             _signal=signal
-                    ): yield b
+                    ):
+                        yield b
                 else:
                     for b in send_message(
                             _signal=signal,
@@ -989,7 +990,7 @@ def serialize_to_buffer(
 
 def client_grpc(
         method,
-        input=None,
+        _input=None,
         timeout=None,
         indices_parser: Union[protobuf.pyext.cpp_message.GeneratedProtocolMessageType, dict] = None,
         partitions_parser: Union[list, dict] = None,
@@ -1011,7 +1012,7 @@ def client_grpc(
     for b in parse_from_buffer(
             request_iterator=method(
                 serialize_to_buffer(
-                    message_iterator=input if input else buffer_pb2.Empty(),
+                    message_iterator=_input if _input else buffer_pb2.Empty(),
                     signal=signal,
                     indices=indices_serializer,
                     partitions_model=partitions_serializer,
