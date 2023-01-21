8607601745a80bf7dcf1d1c616d69e790bf6b86d
Josemi <josemiguel.avellana@m.e-tecnia.es>
2023-01-21 23:46:07 +0100

    set_varint_value

--- src/grpcbigbuffer/block_driver.py
+++ src/grpcbigbuffer/block_driver.py
@@ -44,8 +44,8 @@ def compute_wbp_lengths(tree: Dict[int, Union[Dict, str]], file_list: List[str])
             if isinstance(value, Dict):
                 pruned_length: int = 0
                 for k, v in __rec_compute_wbp_lengths(
-                    _tree=value,
-                    _file_list=_file_list
+                        _tree=value,
+                        _file_list=_file_list
                 ).items():
                     pruned_length += v[1]
                     lengths[k] = (v[0], 0)
@@ -60,44 +60,66 @@ def compute_wbp_lengths(tree: Dict[int, Union[Dict, str]], file_list: List[str])
             print('pruned length size ', len(encode_bytes(pruned_length)))
             print('wbp length ', position_length - pruned_length)
             print('aumented wbp length ', position_length + len(encode_bytes(position_length))
-                    - pruned_length - len(encode_bytes(pruned_length)))
+                  - pruned_length - len(encode_bytes(pruned_length)))
             lengths[key] = (
-                    position_length - pruned_length,
-                    pruned_length + len(encode_bytes(pruned_length))
-                )
+                position_length - pruned_length,
+                pruned_length + len(encode_bytes(pruned_length))
+            )
         return lengths
 
     return {k: v[0] for k, v in __rec_compute_wbp_lengths(_tree=tree, _file_list=file_list).items()}
 
 
-def set_varint_value(varint_pos: int, buffer: bytes, new_value: int) -> bytes:
-    """
-    Sets the value of the varint at the given position in the Protobuf buffer to the given value and returns the modified buffer.
-    """
-    # Convert the given value to a varint and store it in a bytes object
-    varint_bytes = []
-    while True:
-        byte = new_value & 0x7F
-        new_value >>= 7
-        varint_bytes.append(byte | 0x80 if new_value > 0 else byte)
-        if new_value == 0:
-            break
-    varint_bytes = bytes(varint_bytes)
+def set_varint_value(varint_pos: int, buffer: List[Union[bytes, str]], new_value: int):
+    def __set_varint_value(_varint_pos: int, _buffer: bytes, _new_value: int) -> bytes:
+        """
+        Sets the value of the varint at the given position in the Protobuf buffer to the given value and returns the modified buffer.
+        """
+        # Convert the given value to a varint and store it in a bytes object
+        varint_bytes = []
+        while True:
+            byte = _new_value & 0x7F
+            _new_value >>= 7
+            varint_bytes.append(byte | 0x80 if _new_value > 0 else byte)
+            if _new_value == 0:
+                break
+        varint_bytes = bytes(varint_bytes)
 
-    # Calculate the number of bytes to remove from the original varint
-    original_varint_bytes = buffer[varint_pos:]
-    original_varint_length = 0
-    while (original_varint_bytes[original_varint_length] & 0x80) != 0:
+        # Calculate the number of bytes to remove from the original varint
+        original_varint_bytes = _buffer[_varint_pos:]
+        original_varint_length = 0
+        while (original_varint_bytes[original_varint_length] & 0x80) != 0:
+            original_varint_length += 1
         original_varint_length += 1
-    original_varint_length += 1
 
-    # Remove the original varint and append the new one
-    return buffer[:varint_pos] + varint_bytes + buffer[varint_pos + original_varint_length:]
+        # Remove the original varint and append the new one
+        return _buffer[:_varint_pos] + varint_bytes + _buffer[_varint_pos + original_varint_length:]
+
+    """
+    Modify the varint at the given position in the buffer which is composed by a list of bytes and binary files. 
+    The position is based on the concatenation of all the bytes and binary files in the buffer.
+    The function returns the modified buffer as a list of bytes and binary files
+    """
+    offset: int = 0
+    for index, obj in enumerate(buffer):
+        if type(obj) == bytes:
+            obj_size = len(obj)
+            if offset >= varint_pos and varint_pos < offset + obj_size:
+                buffer[index] = __set_varint_value(
+                    _varint_pos=varint_pos-offset,
+                    _buffer=obj,
+                    _new_value=new_value
+                )
+                return
+            offset += obj_size
+        elif type(obj) == str:
+            offset += os.path.getsize(obj)
+    raise Exception('gRPCbb block driver error on set varint value')
 
 
-def generate_new_buffer(lengths: Dict[int, int], buffer: bytes) -> bytes:
+def regenerate_buffer(lengths: Dict[int, int], buffer: List[Union[bytes, str]]) -> List[Union[bytes, str]]:
     for varint_pos, new_value in lengths.items():
-        buffer = set_varint_value(varint_pos, buffer, new_value)
+        set_varint_value(varint_pos, buffer, new_value)
 
     return buffer
 
@@ -110,17 +132,18 @@ def generate_wbp_file(dirname: str):
             Tuple[str, List[int]]
         ]] = json.load(f)
 
-    buffer = b''
+    buffer: List[Union[bytes, str]] = []
     file_list: List[str] = []
     for e in _json:
         if type(e) == int:
             file_list.append(dirname + '/' + str(e))
             with open(dirname + '/' + str(e), 'rb') as file:
-                buffer += file.read()
+                buffer.append(file.read())
         else:
             if type(e) != list or type(e[0]) != str:
                 raise Exception('gRPCbb: Invalid block on _.json file.')
             file_list.append(Enviroment.block_dir + e[0])
+            buffer.append(Enviroment.block_dir + e[0])
 
     blocks: Dict[str, List[int]] = {t[0]: t[1] for t in _json if type(t) == list}
 
@@ -134,7 +157,10 @@ def generate_wbp_file(dirname: str):
     print('\ntree -> ', tree)
     print('\nrecalculated lengths -> ', recalculated_lengths)
 
+    regenerate_buffer(recalculated_lengths, buffer)
+    print('\ngenerated buffer -> ', buffer)
+
     with open(dirname + '/' + WITHOUT_BLOCK_POINTERS_FILE_NAME, 'wb') as f:
         f.write(
-            generate_new_buffer(recalculated_lengths, buffer)
+            b''
         )
