05f700023a5a40860448568c7a3b113a27469a1d
jossemii <jossemii@proton.me>
2023-09-07 20:33:16 +0200

    test block_builder.py with duplicated blocks

--- test/block_builder.py
+++ test/block_builder.py
@@ -361,7 +361,101 @@ class TestBlockBuilder(unittest.TestCase):
                             encode_bytes(get_position_length(_e, buffer)))]
                     )
 
+    def test_typical_complex_object_with_duplicate_blocks(self):
+
+        from grpcbigbuffer.test_pb2 import Test
+
+        block1 = buffer_pb2.Buffer.Block()
+        h = buffer_pb2.Buffer.Block.Hash()
+        h.type = Enviroment.hash_type
+        h.value = sha3_256(b"block1").digest()
+        block1.hashes.append(h)
+
+        if not os.path.isfile(Enviroment.block_dir + sha3_256(b"block1").hexdigest()):
+            with open(Enviroment.block_dir + sha3_256(b"block1").hexdigest(), 'wb') as file:
+                file.write(
+                    b''.join([b'block1' for i in range(100)])
+                )
+
+        block2 = buffer_pb2.Buffer.Block()
+        h = buffer_pb2.Buffer.Block.Hash()
+        h.type = Enviroment.hash_type
+        h.value = sha3_256(b"block2").digest()
+        block2.hashes.append(h)
+
+        if not os.path.isfile(Enviroment.block_dir + sha3_256(b"block2").hexdigest()):
+            with open(Enviroment.block_dir + sha3_256(b"block2").hexdigest(), 'wb') as file:
+                file.write(
+                    b''.join([b'block2' for i in range(100)])
+                )
+
+        a = Test()
+        a.t1 = b''.join([b'bt1' for i in range(1)])
+        a.t2 = block1.SerializeToString()
+
+        b = Test()
+        b.t1 = block2.SerializeToString()
+        b.t2 = b''.join([b'bt2' for i in range(100)])
+        b.t3.CopyFrom(a)
+
+        c = Test()
+        c.t1 = b''.join([b'ct1' for i in range(100)])
+        c.t2 = block1.SerializeToString()
+
+        _object = Test()
+        _object.t1 = b''.join([b'mc1' for i in range(100)])
+        _object.t2 = block2.SerializeToString()
+        _object.t4.append(b)
+        _object.t4.append(c)
+        _object.t5 = b'final'
+
+        object_id, cache_dir = build_multiblock(
+            pf_object_with_block_pointers=_object,
+            blocks=[
+                sha3_256(b"block1").digest(),
+                sha3_256(b"block2").digest(),
+                sha3_256(b"block3").digest()
+            ]
+        )
+        print(f"Block builder ok. test complex obj with duplicate blocks.")
+        # Read the buffer.
+        buffer = b''
+        with open(os.path.join(cache_dir, '_.json'), 'r') as f:
+            _json = json.load(f)
+
+        for element in _json:
+            if type(element) == int:
+                with open(os.path.join(cache_dir, str(element)), 'rb') as f:
+                    block = f.read()
+                    buffer += block
+
+            if type(element) == list:
+                with open(os.path.join(Enviroment.block_dir, element[0]), 'rb') as f:
+                    while True:
+                        block = f.read(1024)
+
+                        if not block:
+                            break
+                        buffer += block
+
+        buff_object = Test()
+        buff_object.ParseFromString(buffer)
+
+        for element in _json:
+            if type(element) == list:
+                for _e in element[1]:
+                    print(
+                        '\n\n',
+                        str(_e) + ' ', get_position_length(_e, buffer),
+                        encode_bytes(get_position_length(_e, buffer)),
+                        buffer[_e:_e + get_position_length(_e, buffer) + len(
+                            encode_bytes(get_position_length(_e, buffer)))]
+                    )
+
 
 if __name__ == '__main__':
+    os.makedirs("__cache__", exist_ok=True)
+    os.makedirs("__block__", exist_ok=True)
     os.system('rm -rf __cache__/*')
+    os.system('rm -rf __block__/*')
     unittest.main()
