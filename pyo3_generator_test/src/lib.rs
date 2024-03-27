b109149550924f4f7260902010851da5a6f0a810
jse07 <jse07@anon.erg>
2024-03-27 19:50:29 +0100

    more investigation for rust generators made

--- pyo3_generator_test/src/lib.rs
+++ pyo3_generator_test/src/lib.rs
@@ -1,9 +1,32 @@
+use futures::pin_mut;
+use futures::stream::{ Stream, StreamExt}; // Importa Stream y StreamExt
+use futures::executor;
+use async_stream::stream;
 use pyo3::prelude::*;
+
+fn zero_to_three() -> impl Stream<Item = u32> {
+    stream! {
+        for i in 0..16 {
+            yield i;
+        }
+    }
+}
+
+async fn sumatory() -> u32 {
+    let mut i: u32 = 0;
+    let s = zero_to_three();
+    pin_mut!(s);
+    while let Some(item) = s.next().await {
+        i += item;
+    }
+    i
+}
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+    let i = executor::block_on(sumatory());
+    return Ok(i.to_string())
 }
 
 /// A Python module implemented in Rust.
